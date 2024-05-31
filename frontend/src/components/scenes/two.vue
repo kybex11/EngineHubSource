4d789c67295da17f15c393d60644a3eaa90406e3
kybex11 <artm.22@internet.ru>
2024-05-31 09:02:45 +0300

    draw sample 2d

--- frontend/src/components/scenes/two.vue
+++ frontend/src/components/scenes/two.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>two</h1>
-</template> 
+    <div>
+        <canvas ref="scene" width="200" height="200"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        const canvas = this.$refs.scene;
+        const ctx = canvas.getContext('2d');
+
+        ctx.beginPath();
+        ctx.arc(100, 100, 50, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+}
+</script>
