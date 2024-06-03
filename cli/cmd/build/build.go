b695c8fcfa988f331dcd726831e79e7a48102a5e
kybex11 <artm.22@internet.ru>
2024-06-03 12:32:38 +0300

    update

--- cli/cmd/build/build.go
+++ cli/cmd/build/build.go
@@ -3,11 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
 	projectName := flag.String("projectName", "", "Specify the project name")
 	flag.Parse()
+
+	cmd := exec.Command("cmd", "/C", "cd ./ && cd ../builder/ && wails init -n "+*projectName+" -t vue-ts")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 
 	fmt.Printf("Building project %s...\n", *projectName)
 }
