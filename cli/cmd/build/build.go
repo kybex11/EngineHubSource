a4c9481e17f79e53061e3cecab4c031f3e724e9f
kybex11 <artm.22@internet.ru>
2024-06-10 19:44:17 +0300

    update

--- cli/cmd/build/build.go
+++ cli/cmd/build/build.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -15,14 +17,14 @@ func main() {
 
 	projectName := *projectSfName + *projectSfName
 
-	fmt.Println("Creating application V2...")
+	color.Blue("Creating application V2...")
 
 	cmdMkdir := exec.Command("powershell", "/C", "New-Item", "-Path", ""+*tPath+"/builder/", "-ItemType", "directory")
 	cmdMkdir.Stdout = os.Stdout
 	cmdMkdir.Stderr = os.Stderr
 	errMkdir := cmdMkdir.Run()
 	if errMkdir != nil {
-		fmt.Println("Error creating directory:", errMkdir)
+		color.Red("Error creating directory:", errMkdir)
 		return
 	}
 	fmt.Println("Directory created successfully")
@@ -31,43 +33,43 @@ func main() {
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error:", err)
+		color.Red("Error:", err)
 		return
 	}
-	fmt.Println("Project created successfully")
+	color.Green("Project created successfully")
 
-	fmt.Println("Cloning project...")
+	color.Blue("Cloning project...")
 	cmd2 := exec.Command("powershell", "/C", "Copy-Item", "-Path", ""+*dPath+""+projectName, "-Destination", ""+*tPath+"/builder/"+projectName, "-Recurse", "-Force")
 	cmd2.Stdout = os.Stdout
 	cmd2.Stderr = os.Stderr
 	err2 := cmd2.Run()
 	if err2 != nil {
-		fmt.Println("Error:", err2)
+		color.Red("Error:", err2)
 		return
 	}
-	fmt.Println("Project cloned successfully")
+	color.Green("Project cloned successfully")
 
-	fmt.Printf("Building project %s...\n", projectName)
+	color.Blue("Building project %s...\n", projectName)
 	cmd3 := exec.Command("wails", "build")
 	cmd3.Dir = "" + *tPath + "/builder/" + projectName
 	cmd3.Stdout = os.Stdout
 	cmd3.Stderr = os.Stderr
 	err3 := cmd3.Run()
 	if err3 != nil {
-		fmt.Println("Error:", err3)
+		color.Red("Error:", err3)
 		return
 	}
-	fmt.Println("Project build successfully")
+	color.Green("Project build successfully")
 
-	fmt.Println("Copying .exe file")
+	color.Blue("Copying .exe file")
 	cmd4 := exec.Command("powershell", "/C", "Copy-Item", "-Path", "./"+projectName+".exe", "-Destination", "../../../../", "-Recurse", "-Force")
 	cmd4.Dir = "" + *tPath + "/builder/" + projectName + "/build/bin"
 	cmd4.Stdout = os.Stdout
 	cmd4.Stderr = os.Stderr
 	err4 := cmd4.Run()
 	if err4 != nil {
-		fmt.Println("Error:", err4)
+		color.Red("Error:", err4)
 		return
 	}
-	fmt.Println("Project build ended")
+	color.Green("Project build ended")
 }
